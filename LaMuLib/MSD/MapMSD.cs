7091f1a7a004c66e4e58d8067daa4af2049372a9
Thomas Goos <goost15@gmail.com>
2017-07-12 04:11:47 +0200

    Final bit of refactoring

--- LaMuLib/MSD/MapMSD.cs
+++ LaMuLib/MSD/MapMSD.cs
@@ -1,4 +1,6 @@
-﻿using LaMuLib.MSD;
+﻿using System.IO;
+using LaMuLib.MSD;
+using LaMuLib.Util;
 
 namespace LaMuLib
 {
@@ -8,5 +10,18 @@ namespace LaMuLib
         public byte GraphicsFilesID;
         public short NumberOfZones;
         public Zone[] Zones;
+        
+        public static MapMSD ParseFile(string path)
+        {
+            var msd = new MapMSD();
+            var reader = new BigEndianBinaryReader(File.OpenRead(path));
+
+            msd.AntimatedTiles = AnimatedTile.FromReader(reader);
+            msd.GraphicsFilesID = reader.ReadByte();
+            msd.NumberOfZones = reader.ReadInt16();
+            msd.Zones = Zone.FromReader(reader,msd);
+            
+            return msd;
+        }
     }
 }
